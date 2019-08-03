9718911e332b1c086f36dbc1a99bb1f580b4df60
Jaeho Lee <jaeho.lee@snu.ac.kr>
2019-08-03 18:37:00 +0900

    :tada: Initial commit

---
+++ LandmarksSwiftUITutorials/SceneDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  SceneDelegate.swift
+//  LandmarksSwiftUITutorials
+//
+//  Created by Jaeho Lee on 2019/08/03.
+//  Copyright © 2019 Jay Lee. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  var window: UIWindow?
+
+  func scene(
+    _ scene: UIScene,
+    willConnectTo session: UISceneSession,
+    options connectionOptions: UIScene.ConnectionOptions)
+  {
+    // Use a UIHostingController as window root view controller
+    if let windowScene = scene as? UIWindowScene {
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = UIHostingController(rootView: ContentView())
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+  }
+}
+
